1eb231aae193a674fc9e0688de951c570651bb81
Ben Blair <benchrisblair@users.noreply.github.com>
2023-08-01 09:17:49 -0700

    Add Signet and RevokedSignet to admin

--- article/admin.py
+++ article/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from article.models import Article
+from signoffs.contrib.signets.models import Signet, RevokedSignet
 
 admin.site.register(Article)
+
+admin.site.register(Signet)
+admin.site.register(RevokedSignet)

8d356c744d04b83d4ef567dc4d641d99b50d7ba4
Ben Blair <benchrisblair@users.noreply.github.com>
2023-08-08 11:54:03 -0700

    Make all fields read-only for RevokedNewsletterSignet

--- article/admin.py
+++ article/admin.py
@@ -5,9 +5,17 @@ from signoffs.contrib.signets.models import Signet, RevokedSignet
 from article.models.models import Article
 from article.models.signets import RevokedNewsletterSignet
 
-
-admin.site.register(Article)
+# Signoffs Models
 
 admin.site.register(Signet)
 admin.site.register(RevokedSignet)
-admin.site.register(RevokedNewsletterSignet)
+
+
+# Article Models
+
+admin.site.register(Article)
+
+
+@admin.register(RevokedNewsletterSignet)
+class RevokedNewsletterSignetAdmin(admin.ModelAdmin):
+    readonly_fields = ['signet', 'user', 'timestamp', 'reason']

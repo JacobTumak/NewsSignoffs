05db3520acaee3e1747f9c1dd478682f437d42f1
Ben Blair <benchrisblair@users.noreply.github.com>
2023-08-14 14:32:48 -0700

    Add Save as Draft feature

--- article/forms.py
+++ article/forms.py
@@ -1,10 +1,21 @@
 from django.forms import ModelForm, TextInput, Textarea
 from django.contrib.auth.forms import UserCreationForm
-from article.models.models import Article, Comment
+from article.models.models import Draft, Article, Comment
 
 
 class SignupForm(UserCreationForm):
     pass
+
+
+class DraftForm(ModelForm):
+    class Meta:
+        model = Draft
+        fields = ['title', 'summary', 'article_text']
+        widgets = {
+            'title': TextInput(attrs={'style': 'width:100%'}),
+            'summary': Textarea(attrs={'rows': 2, 'style': 'width:100%'}),  # Set the rows attribute to 2 for the summary field
+            'article_text': Textarea(attrs={'rows': 10, 'style': 'width:100%'}),
+        }
 
 
 class ArticleForm(ModelForm):

ee0827783aaab4ed9dfbfe2cdd6855e6a8878aec
Ben Blair <benchrisblair@users.noreply.github.com>
2023-08-15 15:29:20 -0700

    Update publish_article_signoff

--- article/signoffs.py
+++ article/signoffs.py
@@ -1,7 +1,4 @@
-from signoffs.signoffs import RevokableSignoff, SignoffRenderer, SignoffUrlsManager
-from signoffs.contrib.signets.signoffs import IrrevokableSignoff
-from signoffs.models import Signet
-
+from signoffs.signoffs import SimpleSignoff, RevokableSignoff, IrrevokableSignoff, SignoffRenderer, SignoffUrlsManager
 
 from article.models.signets import RevokedNewsletterSignet
 
@@ -12,14 +9,10 @@ newsletter_signoff = RevokableSignoff.register(id='newsletter_signoff',
                                                revokeModel=RevokedNewsletterSignet,
                                                urls=SignoffUrlsManager(revoke_url_name='revoke_newsletter'))
 
-publish_article_signoff = RevokableSignoff.register(id='publish_article_signoff',
-                                                    signet=Signet,
+publish_article_signoff = SimpleSignoff.register(id='publish_article_signoff',
                                                     sigil_label='Published by',
                                                     label='I agree to the terms and conditions',
                                                     render=SignoffRenderer(signet_template='signoffs/publish_signet.html',
                                                                            signoff_form_template='signoffs/simple_signoff_form.html',
                                                                            form_context=dict(submit_label='Publish Article',
-                                                                                             help_text='Publishing will make this article viewable to everyone.',
-                                                    )))
-
-
+                                                                                             help_text='Publishing will make this article viewable to everyone.')))
